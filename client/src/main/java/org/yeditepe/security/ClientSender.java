f6c36aef9541f37c5e999d3241ccbd483e5670e7
fuatk <fkarakus@kocfinans.com.tr>
2020-12-07 10:54:48 +0300

    client and server class added

---
+++ client/src/main/java/org/yeditepe/security/ClientSender.java
@@ -0,0 +1,148 @@
+package org.yeditepe.security;
+
+import org.yeditepe.security.utils.ProtocolUtilities;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.file.Paths;
+import java.security.GeneralSecurityException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.ArrayList;
+import java.util.Scanner;
+import javax.crypto.Cipher;
+import javax.crypto.CipherOutputStream;
+import javax.crypto.KeyGenerator;
+import javax.crypto.spec.SecretKeySpec;
+
+public class ClientSender {
+
+    private static String hostName;
+    private static int portNumber;
+
+    private static void sendEcryptedAesKEY(OutputStream out, byte[] publicKey,byte[] aesKey)
+            throws GeneralSecurityException, IOException {
+        Cipher pkCipher = Cipher.getInstance("RSA");
+        PublicKey pk = KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(publicKey));
+        pkCipher.init(Cipher.ENCRYPT_MODE, pk);
+        ByteArrayOutputStream tempByteStream = new ByteArrayOutputStream();
+        CipherOutputStream cipherStream = new CipherOutputStream(tempByteStream, pkCipher);
+        cipherStream.write(aesKey);
+        cipherStream.close();
+        tempByteStream.writeTo(out);
+    }
+
+    private static boolean sendFile(byte[] publicKey, byte[] aesKey, File file)
+            throws FileNotFoundException, IOException, GeneralSecurityException {
+        Socket socket = new Socket(hostName, portNumber);
+        BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream());
+        BufferedInputStream in = new BufferedInputStream(socket.getInputStream());
+        // send header and encrypted AES. AES is encrypted using private RSA key.
+        out.write("FILE TRANSFER\n\n".getBytes("ASCII"));
+        sendEcryptedAesKEY(out,publicKey,aesKey);
+        // Encrypt the name of the file and its size using AES and send it over the socket
+        String fileNameAndSize = new String(file.getName()  + "\n" + file.length() + "\n");
+        ByteArrayInputStream fileInfoStream = new ByteArrayInputStream(fileNameAndSize.getBytes("ASCII"));
+        SecretKeySpec aeskeySpec = new SecretKeySpec(aesKey, "AES");
+        Cipher aesCipher = Cipher.getInstance("AES");
+        aesCipher.init(Cipher.ENCRYPT_MODE, aeskeySpec);
+        CipherOutputStream cipherOutStream = new CipherOutputStream(out, aesCipher);
+        ProtocolUtilities.sendBytes(fileInfoStream,cipherOutStream);
+        // send the the actual file itself and append some bytes so cipher would know it's the end of the file
+        FileInputStream fileStream = new FileInputStream(file);
+        ProtocolUtilities.sendBytes(fileStream,cipherOutStream);
+        out.write(aesCipher.doFinal());
+        out.write("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n".getBytes("ASCII"));
+        out.flush();
+        ArrayList<String> serverResponse = ProtocolUtilities.consumeAndBreakHeader(in);
+        socket.close();
+        if (!serverResponse.get(0).equals("SUCCESS")) {
+            System.err.println("Failed to send file. The Server responded with the following:");
+            for (String msg : serverResponse)
+                System.err.println(msg);
+            return false;
+        }
+        return true;
+    }
+
+    private static byte[] getPublicKey() throws IOException {
+        Socket socket = new Socket(hostName, portNumber);
+        BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream());
+        BufferedInputStream in = new BufferedInputStream(socket.getInputStream());
+        out.write("GET PUBLIC KEY\n\n".getBytes("ASCII"));
+        out.flush();
+        ArrayList<String> headerParts = ProtocolUtilities.consumeAndBreakHeader(in);
+        if (!headerParts.get(0).equals("PUBLIC KEY")) {
+            System.err.println("Failed to obtain public key. The Server responded with the following:");
+            for (String msg : headerParts)
+                System.err.println(msg);
+            System.exit(1);
+        }
+        int keySize = Integer.parseInt(headerParts.get(1));
+        byte[] publicKey = new byte[keySize];
+        in.read(publicKey);
+        socket.close();
+        return publicKey;
+    }
+
+    private static byte[] generateAesKey() throws NoSuchAlgorithmException {
+        byte[] secretAesKey = null;
+        KeyGenerator kgen = KeyGenerator.getInstance("AES");
+        kgen.init(ProtocolUtilities.KEY_SIZE_AES); // AES key length 128 bits (16 bytes)
+        secretAesKey = kgen.generateKey().getEncoded();
+        return secretAesKey;
+    }
+
+    public static void execute(String[] args) {
+
+        hostName = "localhost";
+
+        portNumber = 8080;
+
+        System.out.println("Using host name: "+hostName + " and port number: " + portNumber + "...");
+
+        byte[] publicRsaKey, secretAesKey;
+
+        File[] currentDirFiles = new File(Paths.get(".").toAbsolutePath().toString()).listFiles();
+
+        System.out.println("Available files in the current directory:");
+        for (File f : currentDirFiles) {
+            String fileName = f.getName();
+            if (fileName.charAt(0) == '.') // ignore hidden files.
+                continue;
+            System.out.println(fileName);
+        }
+        try {
+            publicRsaKey = getPublicKey();
+            secretAesKey = generateAesKey();
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter the name of the file to send: ");
+            String fileName = scanner.next();
+            boolean isSuccessful = sendFile(publicRsaKey, secretAesKey, new File(fileName));
+            if (isSuccessful){
+                System.out.println("File was successfully sent.");
+            } else {
+                System.out.println("File was not sent.");
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found.");
+        } catch (IOException e) {
+            System.err.println("There was an error connecting to the server.");
+        } catch (NoSuchAlgorithmException e) {
+            System.err.println("Failed to generate AES key.");
+        } catch (GeneralSecurityException e) {
+            System.err.println("Unknown security error.");
+        }
+    }
+}
---
+++ server/src/main/java/org/yeditepe/security/ServerReceiver.java
@@ -0,0 +1,171 @@
+package org.yeditepe.security;
+
+import org.yeditepe.security.utils.ProtocolUtilities;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.security.GeneralSecurityException;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.ArrayList;
+import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
+import javax.crypto.spec.SecretKeySpec;
+
+public class ServerReceiver {
+
+    private static final int PORT = 8080;
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("The server is running.");
+        ServerSocket listener = new ServerSocket(PORT);
+
+        try {
+
+            new Handler(listener.accept()).start();
+
+        } finally {
+            listener.close();
+        }
+    }
+
+    private static class Handler extends Thread {
+        private Socket socket;
+        private InputStream in;
+        private OutputStream out;
+
+        private void sendPublicKey() throws IOException {
+            StringBuilder messageHeader = new StringBuilder();
+            messageHeader.append("PUBLIC KEY\n");
+            File publicKeyFile = new File("public.der");
+            messageHeader.append(publicKeyFile.length() + "\n\n");
+            out.write(messageHeader.toString().getBytes("ASCII"));
+            out.write(Files.readAllBytes(publicKeyFile.toPath()));
+            out.flush();
+        }
+
+        private void sendErrorMessage(String msg) {
+            try {
+                msg = "ERROR\n" + msg + "\n\n";
+                out.write(msg.getBytes("ASCII"));
+            } catch (IOException e) {
+                System.out.println("Failed to send an error message to client.");
+                System.exit(1);
+            }
+        }
+
+        private byte[] readAndDecryptAesKey(byte[] privateKeyFile) throws GeneralSecurityException, IOException {
+            // read the encrypted AES key from the socket
+            byte[] encryptedAesKey = new byte[ProtocolUtilities.KEY_SIZE_AES * 2];
+            in.read(encryptedAesKey);
+            // put the private RSA key in the appropriate data structure
+            PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(privateKeyFile);
+            PrivateKey privateKey = KeyFactory.getInstance("RSA").generatePrivate(privateKeySpec);
+            // Decipher the AES key using the private RSA key
+            Cipher pkCipher = Cipher.getInstance("RSA");
+            pkCipher.init(Cipher.DECRYPT_MODE, privateKey);
+            CipherInputStream cipherInputStream = new CipherInputStream(new ByteArrayInputStream(encryptedAesKey), pkCipher);
+            byte[] aesKey = new byte[ProtocolUtilities.KEY_SIZE_AES / 8];
+            cipherInputStream.read(aesKey);
+            cipherInputStream.close();
+            return aesKey;
+        }
+
+        private String scanLineFromCipherStream(CipherInputStream cstream) throws IOException {
+            StringBuilder line = new StringBuilder();
+            char c;
+            while ((c = (char) cstream.read()) != '\n') {
+                line.append(c);
+            }
+            return line.toString();
+        }
+
+        private File receiveFile(byte[] aesKey) throws GeneralSecurityException, IOException {
+            Cipher aesCipher = Cipher.getInstance("AES");
+            SecretKeySpec aeskeySpec = new SecretKeySpec(aesKey, "AES");
+            aesCipher.init(Cipher.DECRYPT_MODE, aeskeySpec);
+            CipherInputStream cipherInputStream = new CipherInputStream(in, aesCipher);
+            String fileName = scanLineFromCipherStream(cipherInputStream);
+            String fileSize = scanLineFromCipherStream(cipherInputStream);
+            File receivedFile = new File(fileName.toString());
+            FileOutputStream foStream = new FileOutputStream(receivedFile);
+            ProtocolUtilities.sendBytes(cipherInputStream, foStream, Long.parseLong(fileSize));
+            foStream.flush();
+            foStream.close();
+            return receivedFile;
+        }
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+
+        public void run() {
+            String command;
+            try {
+                in = new BufferedInputStream(socket.getInputStream());
+                out = new BufferedOutputStream(socket.getOutputStream());
+                ArrayList<String> headerParts = ProtocolUtilities.consumeAndBreakHeader(in);
+                command = headerParts.get(0);
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.err.println("Connection to client dropped.");
+                return;
+            } catch (NullPointerException e) {
+                System.err.println("Unable to read command from client");
+                return;
+            }
+            switch (command) {
+                case "GET PUBLIC KEY":
+                    try {
+                        sendPublicKey();
+                        System.out.println("Sent public key!");
+                    } catch (IOException e) {
+                        System.err.println("Connection to client dropped. Failed to send public key.");
+                    }
+                    break;
+                case "FILE TRANSFER":
+                    byte[] privateRsaKey;
+                    try {
+                        privateRsaKey = Files.readAllBytes(new File("private.der").toPath());
+                    } catch (IOException e) {
+                        sendErrorMessage("SERVER ERROR");
+                        System.err.println("Server failed to open private key file.");
+                        return;
+                    }
+                    try {
+                        byte[] aesKey = readAndDecryptAesKey(privateRsaKey);
+                        File file = receiveFile(aesKey);
+                        System.out.println("Received File");
+                        System.out.println("Name: " + file.getName());
+                        System.out.println("Size:" + file.length());
+                        out.write("SUCCESS\nsuccessful transmission\n\n".getBytes("ASCII"));
+                        out.flush();
+                        socket.close();
+                    } catch (GeneralSecurityException e) {
+                        sendErrorMessage("Failed to decrypt AES key and/or file content.");
+                        System.err.println("Server failed to decrypt AES key and/or file content.");
+                        return;
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        System.err.println("Connection to client dropped.");
+                        return;
+                    }
+                    break;
+                default:
+                    sendErrorMessage("INVALID COMMAND");
+                    System.out.println("Invalid command detected: " + command);
+            }
+        }
+    }
+}
